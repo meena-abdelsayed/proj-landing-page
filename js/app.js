7a4b083d271f4170eb038e1ec1bfb909648a7818
Meena-Abdo <meena.abdelsayed@gmail.com>
2020-09-17 12:55:09 +0200

    feat: building navbar dynamically with js

--- js/app.js
+++ js/app.js
@@ -17,8 +17,9 @@
  * Define Global Variables
  * 
 */
-const navbarList = document.getElementById('#navbar__list'); // unordered list items
+const navbarList = document.getElementById('navbar__list'); // unordered list items
 const sections = document.querySelectorAll('section'); // all existing sections
+const sectionTitle = document.querySelectorAll('h2');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -34,7 +35,17 @@ const sections = document.querySelectorAll('section'); // all existing sections
 */
 
 // build the nav
-
+for (let i = 0; i < sections.length; i++) {
+    const listItems = document.createElement('li');
+    const anchorElem = document.createElement('a');
+    const anchorIdText = "section" + (i + 1);
+    const anchorTextElem = document.createTextNode(sectionTitle[i].textContent);
+    anchorElem.appendChild(anchorTextElem);
+    anchorElem.setAttribute('href', ('#' + anchorIdText));
+    anchorElem.classList.add('menu__link');
+    listItems.appendChild(anchorElem);
+    navbarList.appendChild(listItems);
+}
 
 // Add class 'active' to section when near top of viewport
 
